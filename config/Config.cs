de01951a4dd84fb0b6608369c698b3796d356499
Serhii Tymoshenko <serhij.tymoschenko@gmail.com>
2025-05-12 07:33:23 +0200

    Added Config

---
+++ config/Config.cs
@@ -0,0 +1,11 @@
+namespace rca.config;
+
+public class Config
+{
+    public static ulong LOG_CHANNEL_ID = ulong.Parse(Environment.GetEnvironmentVariable("LOG_CHANNEL_ID"));
+    public static string DS_API_KEY = Environment.GetEnvironmentVariable("DS_API_KEY");
+    public static string POLYSCAN_API_KEY = Environment.GetEnvironmentVariable("POLYSCAN_API_KEY");
+    public static string FIREBASE_PROJECT_ID = Environment.GetEnvironmentVariable("FIREBASE_PROJECT_ID");
+    public static string FIREBASE_CREDENTIALS = Environment.GetEnvironmentVariable("FIREBASE_CREDENTIALS");
+    public static string REDDIT_API_KEY = Environment.GetEnvironmentVariable("REDDIT_API_KEY");
+}

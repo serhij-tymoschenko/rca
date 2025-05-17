b0798cdecf61ac945e551f0b60cad756378bb4cd
Serhii Tymoshenko <serhij.tymoschenko@gmail.com>
2025-05-17 10:45:41 +0200

    Fix

--- config/Config.cs
+++ config/Config.cs
@@ -1,7 +1,7 @@
 namespace rca.config;
 
 public class Config
-{
+{ 
     public static ulong LOG_CHANNEL_ID = ulong.Parse(Environment.GetEnvironmentVariable("LOG_CHANNEL_ID"));
     public static string DS_API_KEY = Environment.GetEnvironmentVariable("DS_API_KEY");
     public static string POLYSCAN_API_KEY = Environment.GetEnvironmentVariable("POLYSCAN_API_KEY");
